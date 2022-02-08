5cc2019d2f631484ce11ae37036e0d0f0b36c1c4
Elijah Seabock <elijah@Ians-MacBook-Pro-3.local>
2022-02-07 20:24:57 -0800

    added common loss and categorical cross-entropy loss class

--- neural_network.py
+++ neural_network.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
-# Our dense layer will begin with two methods
+
 class Layer_Dense:
 
     def __init__(self, n_inputs, n_neurons):
@@ -24,9 +24,6 @@ class Activation_ReLU:
         # Calculate output values from inputs
         self.output = np.maximum(0, inputs)
 
-# softmax activatation
-
-
 class Activation_Softmax:
 
     # forward pass
@@ -39,3 +36,47 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
         self.output = probabilities
+ 
+# Common loss
+class Loss:
+
+    # Calculates the dataand regularization losses
+    # given model output and ground truth values
+    def calculate(self, output, y):
+
+        # Calculate samples losses
+        # forward called when inherited
+        sample_losses = self.forward(output, y)
+
+        # Calculate mean loss
+        data_loss = np.mean(sample_losses)
+
+        # Return loss
+        return data_loss
+
+# inherits from Loss class
+class CategoricalCrossEntropy(Loss):
+
+    # forward pass
+    def forward(self, y_pred, y_true):
+
+        # Number of samples in batch
+        samples = len(y_pred)
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not introduce a bias in any direction
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Probabilities for target values - only if categorical labels
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        # Mask values - only for one-hot encoded labels
+        elif len(class_targets.shape) == 2:
+        correct_confidences = np.sum(
+            y_pred_clipped * y_true,
+            axis=1
+        )
+
+        # Losses
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
